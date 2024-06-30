f0aa547ca0ed6dce065204d56cbead7d2ba6bf6a
getoffdeez <111947685+getoffdeez@users.noreply.github.com>
2024-06-30 12:24:49 -0700

    quick update to room images

--- components/card-base.tsx
+++ components/card-base.tsx
@@ -8,7 +8,7 @@ import Image from 'next/image'
 const CardBase= () => {
   return (
   <div className="flex flex-col gap-4 border-4 border-b-8 border-blue-500 shadow-lg p-4 bg-white rounded-md transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl hover:rotate-2 min-w-56">
-    <Image src="/summer/frame-blue.png" alt={"Onchain Summer"} width={200} height={200}       
+    <Image src="/summer/bald-jesse.png" alt={"Onchain Summer"} width={200} height={200}       
     />
     <h3 className="text-teal-900 text-center typewriter font-bold">OnChain Summer</h3>
     <Link href={'/onchain-summer'}>
--- components/card-noun.tsx
+++ components/card-noun.tsx
@@ -7,9 +7,9 @@ import Image from 'next/image'
 const CardNoun= () => {
   return (
     <div className="flex flex-col gap-4 border-4 border-b-8 border-yellow-500 shadow-lg p-4 bg-white rounded-md transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl hover:rotate-2 min-w-56">
-    <Image src="/summer/frame-yellow.png"  alt={"Yellow Collective"} width={200} height={200}       
+    <Image src="/yellow-collective.png"  alt={"Yellow Collective"} width={200} height={200}       
     />
-    <h3 className="text-black text-center typewriter">Yellow Collective</h3>
+    <h3 className="text-teal-900 text-center typewriter font-bold">Yellow Collective</h3>
     <Link href={'/nouns'}>
       <Button variant={'secondary'}>Let's Play</Button>
     </Link>
