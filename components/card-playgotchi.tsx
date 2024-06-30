2d9a0bafd3e8d55b2147e04627bf8f55ae9f569f
getoffdeez <111947685+getoffdeez@users.noreply.github.com>
2024-06-30 14:05:14 -0700

    updated playground images

--- components/card-playgotchi.tsx
+++ components/card-playgotchi.tsx
@@ -13,7 +13,7 @@ const inter = Inter({
 const CardPlaygotchi= () => {
   return (
     <div className="flex flex-col gap-4 border-4 border-b-8 border-[#4FFF14] shadow-lg p-4 bg-white rounded-md transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl hover:rotate-2 min-w-56">
-      <Image src="/playgotchi/kai.png" alt={"Kai"} width={200} height={200}       
+      <Image src="/playgotchi.png" alt={"Kai"} width={200} height={200}       
       />
       <h3 className="text-teal-900 text-center typewriter font-bold">Playgotchi</h3>
       <Link href={'/playgotchi'}>
